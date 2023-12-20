e47e1a0416d88b48b0cf73aea9afc3d7810d305e
Jubair <jubair.ahmed7946@gmail.com>
2023-12-21 04:16:54 +0600

    Add: dashboard route added

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom"
+import Container from "../../components/Shared/Container"
 
 
 const Dashboard = () => {
   return (
     <div>
-      <Link to="/"  className="absolute top-4 left-4 px-4 py-2 rounded-lg font-bold bg-gradient-to-r from-violet-600 to-fuchsia-600 text-white border-none">inTask</Link>
-      <h1>dashboard</h1>
+      <Container>
+
+      <Link to="/"  className="absolute top-5 px-4 py-2 rounded-lg font-bold bg-gradient-to-r from-violet-600 to-fuchsia-600 text-white border-none">inTask</Link>
+      
+      </Container>
     </div>
   )
 }
