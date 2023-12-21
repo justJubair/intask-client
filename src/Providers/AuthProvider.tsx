5fa20bddd16465c3e44bf6c01d4da0e9e22bb49b
Jubair <jubair.ahmed7946@gmail.com>
2023-12-21 22:45:07 +0600

    Add: google sign in added

--- src/Providers/AuthProvider.tsx
+++ src/Providers/AuthProvider.tsx
@@ -1,33 +1,62 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config.tsx";
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   UserCredential,
 } from "firebase/auth";
 interface AuthInfo {
   createUser: (email: string, password: string) => Promise<UserCredential>;
   loginUser: (email: string, password: string) => Promise<UserCredential>;
+  googleSignIn: () => Promise<UserCredential>;
+  user: object | null;
 }
 
 export const AuthContext = createContext<AuthInfo | null>(null);
+
+// google provider
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState({} || null);
+
   // create new user
   const createUser = (email: string, password: string) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // login user
-  const loginUser = (email:string, password:string)=>{
-    return signInWithEmailAndPassword(auth, email, password)
-  }
+  const loginUser = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
- 
+  // login with google
+  const googleSignIn = ()=>{
+    return signInWithPopup(auth, googleProvider)
+  } 
+
+  // setup an observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("observer", currentUser);
+
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const authInfo: AuthInfo = {
+    user,
     createUser,
-    loginUser
+    loginUser,
+    googleSignIn,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/components/Shared/SocialLogin.tsx
+++ src/components/Shared/SocialLogin.tsx
@@ -1,15 +1,33 @@
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import useAuth from "../../hooks/useAuth";
+import toast, { Renderable, Toast, ValueFunction } from "react-hot-toast";
 const SocialLogin = () => {
-    return(
-        <div className="flex justify-center items-center gap-4 mb-6">
-            <button className="btn flex items-center ">
-                <h3 className="font-medium text-lg">Login with</h3>
-                <FaGoogle size={20}/>
-            </button>
-            <button className="btn flex items-center">
-                <h3 className="font-medium text-lg">Login with</h3>
-                <FaGithub size={23}/>
-            </button>
-        </div>
-    )}
+  const { googleSignIn } = useAuth();
+
+  const handleGoogleLogin = () => {
+    googleSignIn()
+      .then((result: { user: object }) => {
+        if (result?.user) {
+          toast.success("Logged in");
+        }
+      })
+      .catch(
+        (error: { message: Renderable | ValueFunction<Renderable, Toast> }) => {
+          toast.error(error.message);
+        }
+      );
+  };
+  return (
+    <div className="flex justify-center items-center gap-4 mb-6">
+      <button onClick={handleGoogleLogin} className="btn flex items-center ">
+        <h3 className="font-medium text-lg">Login with</h3>
+        <FaGoogle size={20} />
+      </button>
+      <button className="btn flex items-center">
+        <h3 className="font-medium text-lg">Login with</h3>
+        <FaGithub size={23} />
+      </button>
+    </div>
+  );
+};
 export default SocialLogin;
